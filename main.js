7fa097f9d0c486d270d7dc16755811e41f3abc55
Taija Mitronen <taiski.92@gmail.com>
2024-01-11 11:33:52 +0200

    Koodin räpellystä

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-/** @format */
-const canvas = document.getElementById("gameBoard");
-const ctx = canvas.getContext("2d");
+let canvas = document.getElementById("gameBoard");
+let ctx = canvas.getContext("2d");
 // ALEKSIN RIVIT
 
 
@@ -19,4 +18,25 @@ const ctx = canvas.getContext("2d");
 
 
 
-// TAIJAN RIVIT 
+// TAIJAN RIVIT
+/* W: 800px    H: 600px
+*/
+// Tämä toimii testikuvana 
+// let char = 'photos/testPhoto.png';
+
+function startGame() {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(150, 150, 50, 50);
+    myGamePiece = new component(50, 50, "blue", 10, 120);
+}
+
+// MOVEMENT
+// Näitä muokattava oman näköiseksi
+document.addEventListener("keydown", function (event) {
+    keystate_Player[event.key] = true;
+})
+document.addEventListener("keyup", function (event) {
+    keyState_Player[event.key] = false;
+});
+
+// MOVEMENT KEYS "W A S D"
